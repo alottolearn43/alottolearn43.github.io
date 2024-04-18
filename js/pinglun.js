fb02c073776eef3470572e00e613906e4a62b26b
alottolearn43 <dudu_txjx@163.com>
2024-04-18 21:59:08 +0800

    Site updated: 2024-04-18 21:59:08

---
+++ js/pinglun.js
@@ -0,0 +1,15 @@
+new Valine({
+    el: source/_posts/第二篇文章的标题.md ,
+    appId: kn3BDjau4t9OdEoozxOuDEoK-MdYXbMMI,
+    appKey: KJDZrLfjnr98M5YhPTnnCzQQ
+});
+
+// Use import
+import Valine from 'valine';
+// or Use require
+const Valine = require('valine');
+
+new Valine({
+    el:source/_posts/第二篇文章的标题.md,
+    // other config
+})
