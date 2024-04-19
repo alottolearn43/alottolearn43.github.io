fc8aec0777bec0b36459ad7e7c058aa4eb59af74
alottolearn43 <dudu_txjx@163.com>
2024-04-19 12:38:21 +0800

    Site updated: 2024-04-19 12:38:21

--- js/pinglun.js
+++ js/pinglun.js
@@ -12,14 +12,14 @@ const Valine = require('valine');
 new Valine({
     el:'#vcomments',
     // other config
-    // appId: 'kn3BDjau4t9OdEoozxOuDEoK-MdYXbMMI', // 你的AppId  
-    // appKey: 'KJDZrLfjnr98M5YhPTnnCzQQ', // 你的AppKey  
-    // notify: false, // 邮件通知，默认为false  
-    // verify: false, // 验证码功能，默认为false  
-    // avatar: 'mm', // 头像设置，默认'mm'，使用leancloud的默认头像；设置为'cdn'则使用Gravatar头像  
-    // placeholder: '说点什么吧...', // 输入框占位符  
-    // visitor: true, // 游客评论，默认为true  
-    // pageSize: 10, // 分页大小，默认为10  
-    // lang: 'zh-cn' // 语言设置，默认为'zh-cn'  
-    // // 其他更多配置...  
+    appId: 'kn3BDjau4t9OdEoozxOuDEoK-MdYXbMMI', // 你的AppId  
+    appKey: 'KJDZrLfjnr98M5YhPTnnCzQQ', // 你的AppKey  
+    notify: false, // 邮件通知，默认为false  
+    verify: false, // 验证码功能，默认为false  
+    avatar: 'mm', // 头像设置，默认'mm'，使用leancloud的默认头像；设置为'cdn'则使用Gravatar头像  
+    placeholder: '说点什么吧...', // 输入框占位符  
+    visitor: true, // 游客评论，默认为true  
+    pageSize: 10, // 分页大小，默认为10  
+    lang: 'zh-cn' // 语言设置，默认为'zh-cn'  
+    // 其他更多配置...  
 })
